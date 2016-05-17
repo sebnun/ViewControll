640dc56740717e4e16c4df1a302eeb7655c148a4
Sebastian <sebnun@gmail.com>
2016-05-17 20:36:49 -0300

    Implemented functionality

--- ViewControll/AppDelegate.swift
+++ ViewControll/AppDelegate.swift
@@ -10,18 +10,30 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
+    
+    var window: NSWindow?
+    
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let flowViewController = ImageViewController()
+        flowViewController.title = "Flow"
+        flowViewController.image = NSImage(named: NSImageNameFlowViewTemplate)
+        let columnViewController = ImageViewController()
+        columnViewController.title = "Column"
+        columnViewController.image = NSImage(named: NSImageNameColumnViewTemplate)
+        
+        let tabViewController = NSTabViewController()
+        tabViewController.addChildViewController(flowViewController)
+        tabViewController.addChildViewController(columnViewController)
+        
+        let window = NSWindow(contentViewController: tabViewController)
+        window.makeKeyAndOrderFront(self)
+        self.window = window
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
-
---
+++ ViewControll/ImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ImageViewController.swift
+//  ViewControll
+//
+//  Created by Sebastian on 5/17/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import Cocoa
+
+class ImageViewController: NSViewController {
+    
+    var image: NSImage?
+    
+    override var nibName: String? {
+        return "ImageViewController"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
